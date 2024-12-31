dcda14fd9f73f83a6926c32627c81862e848e05c
Mike Rybka <merybka@gmail.com>
2024-12-31 14:38:20 -0500

    Remove bucket existence check

--- client.go
+++ client.go
@@ -34,17 +34,10 @@ func (c *Client) CreateBucket(ctx context.Context, name string) error {
 	return c.minioClient.MakeBucket(ctx, name, minio.MakeBucketOptions{})
 }
 
-func (c *Client) BucketExists(ctx context.Context, name string) (bool, error) {
-	return c.minioClient.BucketExists(ctx, name)
-}
-
 func (c *Client) Bucket(ctx context.Context, name string) (*Bucket, error) {
-	ok, _ := c.BucketExists(ctx, name)
-	if !ok {
-		err := c.CreateBucket(ctx, name)
-		if err != nil {
-			return nil, fmt.Errorf("creating bucket: %s", err)
-		}
+	err := c.CreateBucket(ctx, name)
+	if err != nil {
+		return nil, fmt.Errorf("creating bucket: %s", err)
 	}
 	return &Bucket{
 		minioClient: c.minioClient,
@@ -64,6 +57,20 @@ func (c *Client) Put(ctx context.Context, path string, data []byte) error {
 		return err
 	}
 	return bucket.Put(ctx, objectName, data)
+}
+
+func (c *Client) Get(ctx context.Context, path string) ([]byte, error) {
+	p := parsePath(path)
+	if len(p) < 2 {
+		return nil, fmt.Errorf("path must be at least 2 parts")
+	}
+	bucketName := p[0]
+	objectName := strings.Join(p[1:], "/")
+	bucket, err := c.Bucket(ctx, bucketName)
+	if err != nil {
+		return nil, err
+	}
+	return bucket.Get(ctx, objectName)
 }
 
 func parsePath(s string) []string {
