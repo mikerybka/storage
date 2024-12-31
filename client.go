2ae9b3ca676c25dcff519f3aeb6531231a97483e
Mike Rybka <merybka@gmail.com>
2024-12-31 13:11:46 -0500

    Overhaul

--- client.go
+++ client.go
@@ -1,72 +1,46 @@
 package storage
 
 import (
-	"io"
-	"net/http"
+	"bytes"
+	"context"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
+func NewClient(accessKey, secretKey, endpointURL string) *Client {
+	c, err := minio.New(endpointURL, &minio.Options{
+		Secure: true,
+		Creds: credentials.NewStaticV4(
+			accessKey,
+			secretKey,
+			"",
+		),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return &Client{
+		minioClient: c,
+	}
+}
+
 type Client struct {
-	AccessKey   string
-	SecretKey   string
-	EndpointURL string
+	minioClient *minio.Client
 }
 
-// Read downloads the content from the specified path and writes it to the provided io.Writer.
-func (sc *Client) Read(w io.Writer, path string) (int64, error) {
-	url := sc.EndpointURL + "/" + path
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return 0, err
-	}
-
-	req.SetBasicAuth(sc.AccessKey, sc.SecretKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return 0, &httpError{StatusCode: resp.StatusCode, Status: resp.Status}
-	}
-
-	return io.Copy(w, resp.Body)
+func (c *Client) CreateBucket(ctx context.Context, name string) error {
+	return c.minioClient.MakeBucket(ctx, name, minio.MakeBucketOptions{})
 }
 
-// Write uploads the content from the provided io.Reader to the specified path.
-func (sc *Client) Write(path string, r io.Reader) error {
-	url := sc.EndpointURL + "/" + path
-
-	req, err := http.NewRequest("PUT", url, r)
-	if err != nil {
-		return err
-	}
-
-	req.SetBasicAuth(sc.AccessKey, sc.SecretKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
-		return &httpError{StatusCode: resp.StatusCode, Status: resp.Status}
-	}
-
-	return nil
-}
-
-// httpError is used to handle HTTP response errors.
-type httpError struct {
-	StatusCode int
-	Status     string
-}
-
-func (e *httpError) Error() string {
-	return "HTTP error: " + e.Status
+func (c *Client) Put(ctx context.Context, bucket, name string, data []byte) error {
+	b := bytes.NewBuffer(data)
+	_, err := c.minioClient.PutObject(ctx,
+		bucket,
+		name,
+		b,
+		int64(b.Len()),
+		minio.PutObjectOptions{},
+	)
+	return err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mikerybka/storage
 
 go 1.23.4
+
+require github.com/minio/minio-go v6.0.14+incompatible
+
+require (
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+)
